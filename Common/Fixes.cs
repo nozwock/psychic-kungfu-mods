a82310b8eb82c7e426e02a246b03ba34843cbf4d
nozwock <57829219+nozwock@users.noreply.github.com>
2026-05-31 22:25:29 +0530

    Fix KeyValuePair deconstructing for older target framework

--- Common/Fixes.cs
+++ Common/Fixes.cs
@@ -4,3 +4,23 @@ namespace System.Runtime.CompilerServices
 {
     internal static class IsExternalInit { }
 }
+
+namespace System.Collections.Generic
+{
+    /// <summary>
+    /// Fixes KeyValuePair deconstructing for net472.
+    /// <para/>
+    /// https://www.interact-sw.co.uk/iangblog/2018/04/12/deconstruct-keyvaluepair
+    /// </summary>
+    public static class KeyValuePairExtensions
+    {
+        public static void Deconstruct<TKey, TValue>(
+            this KeyValuePair<TKey, TValue> kvp,
+            out TKey key,
+            out TValue value)
+        {
+            key = kvp.Key;
+            value = kvp.Value;
+        }
+    }
+}
