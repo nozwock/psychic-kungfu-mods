78ccd52db356901169ab64c2ca108c9bacfdc7fe
nozwock <57829219+nozwock@users.noreply.github.com>
2026-06-04 22:53:14 +0530

    Re-implement game's FileWindow (Load page) using SaveMetadata
    This makes the list load instant in comparison to decrypting and
    deserializing all save files just to show a list of them. Worse, the
    game does it again-and-again when switching between different save-type
    tabs and more.
    
    Now, small save metadata files are read to build the list instead and
    they're only read only once on opening the Load page.
    
    Reduced time taken to load Quicksave menu list from ~3 seconds for ~120
    quicksaves to few tens of milliseconds.

--- VanillaPlus/Patches.cs
+++ VanillaPlus/Patches.cs
@@ -1,11 +1,14 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using System.Reflection.Emit;
 using System.Text.RegularExpressions;
 using Common;
 using Common.Extensions;
 using HarmonyLib;
 using UnityEngine;
+using UnityEngine.UI;
 using VanillaPlus.Save;
 
 namespace VanillaPlus.Patches;
@@ -59,6 +62,263 @@ internal static class SaveManager_Recent_Patch
         // null-forgiving because .Recent already returns null with the non-nullable type
         __result = __instance.ReadRecentSaveData()!;
         return false;
+    }
+}
+
+[HarmonyPatch]
+internal static class FileWindow_Patch
+{
+    private static Dictionary<SaveEnum, List<SaveMetadata?>> _saveInfos = [];
+    private static SaveMetadata? _selected;
+    private static MethodInfo? _refreshScrollMethod;
+    private static MethodInfo? _onCloseMethod;
+
+    [HarmonyReversePatch]
+    [HarmonyPatch(typeof(WindowBase), nameof(WindowBase.OnAwake))]
+    private static void Call_WindowBase_OnAwake(WindowBase instance) =>
+        throw new NotImplementedException();
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(FileWindow), nameof(FileWindow.OnAwake))]
+    private static bool FileWindow_OnAwake_Prefix(FileWindow __instance)
+    {
+        var self = __instance;
+
+        _refreshScrollMethod ??= AccessTools.Method(
+            typeof(FileWindow),
+            nameof(FileWindow.RefreshScroll)
+        );
+        _onCloseMethod ??= AccessTools.Method(typeof(FileWindow), nameof(FileWindow.OnClose));
+
+        // Note: Don't use reflection to do this, it'd just call FileWindow.OnAwake instead
+        Call_WindowBase_OnAwake(self);
+
+        var goTable = self.transform.GetComponent<GoTable>();
+
+        var folderButton = goTable.GetNode<UIButton>("Folder_UIButton");
+        folderButton.SetLeftClickEvent(() =>
+        {
+            Application.OpenURL("file://" + SaveManager.Instance.SavePath);
+        });
+
+        var backButton = goTable.GetNode<UIButton>("Back_UIButton");
+        backButton.SetLeftClickEvent(() =>
+        {
+            _onCloseMethod.Invoke(self, []);
+        });
+
+        var loadButton = goTable.GetNode<UIButton>("Load_UIButton");
+        self.m_nextGo = loadButton.transform.parent.gameObject;
+        loadButton.SetLeftClickEvent(() =>
+        {
+            var save = _selected?.ReadSaveData();
+            if (save != null)
+                SaveManager.Instance.Load(save);
+        });
+
+        var scrollView = goTable.GetNode<AillieoUtils.ScrollView>("ScrollView_ScrollView");
+        self.m_scrollView = scrollView;
+        scrollView.SetUpdateFunc(
+            (index, rect) =>
+            {
+                var goTable = rect.GetComponent<GoTable>();
+                var info = _saveInfos[self.m_saveEnum][index];
+                var label = goTable.GetNode<Text>("Num_Text");
+
+                if (self.m_saveEnum == SaveEnum.手动)
+                {
+                    label.transform.parent.gameObject.SetActive(value: true);
+                    label.text = (index + 1).ToString();
+                }
+                else
+                {
+                    label.transform.parent.gameObject.SetActive(value: false);
+                }
+
+                var emptySlotGo = goTable.GetNode<RectTransform>("No_RectTransform").gameObject;
+                var slotToggle = goTable.GetNode<UIToggle>("Have_UIToggle");
+
+                if (info == null)
+                {
+                    slotToggle.gameObject.SetActive(value: false);
+                    emptySlotGo.gameObject.SetActive(value: true);
+                    return;
+                }
+
+                slotToggle.gameObject.SetActive(value: true);
+                emptySlotGo.gameObject.SetActive(value: false);
+                slotToggle.SetLeftClickEvent(
+                    (isOn) =>
+                    {
+                        if (isOn)
+                        {
+                            _selected = info;
+                        }
+                    }
+                );
+
+                var renameButton = goTable.GetNode<UIButton>("Rename_UIButton");
+                renameButton.SetLeftClickEvent(() =>
+                {
+                    UIUtlils.OpenRenameWindow(
+                        LanguageUtils.GetTextDef(23),
+                        info.Name,
+                        (name) =>
+                        {
+                            var save = info.ReadSaveData();
+                            if (save == null)
+                                return false;
+
+                            SaveManager.Instance.ChangeName(save, name);
+                            _saveInfos[self.m_saveEnum][index] = save.ToMetadata();
+                            _refreshScrollMethod.Invoke(self, []);
+
+                            return true;
+                        }
+                    );
+                });
+                renameButton.gameObject.SetActive(self.m_saveEnum != SaveEnum.自动);
+
+                var gameTimeLabel = goTable.GetNode<Text>("GameTime_Text");
+                gameTimeLabel.text = $"{info.GameTime / 3600f:F1}h";
+
+                var difficultyLabel = goTable.GetNode<Text>("Dif_Text");
+                difficultyLabel.text = LanguageUtils.GetDifficultName(info.Difficulty);
+
+                var taskLabel = goTable.GetNode<Text>("Task_Text");
+                if (info.ShowMainId > 0)
+                {
+                    var taskData = DBLoad.Task.Get(info.ShowMainId);
+                    taskLabel.text = LanguageUtils.GetStr(taskData.m_name);
+                }
+                else
+                {
+                    taskLabel.text = string.Empty;
+                }
+
+                var saveNameLabel = goTable.GetNode<Text>("Name_Text");
+                saveNameLabel.text = info.Name;
+
+                var saveTimeLabel = goTable.GetNode<Text>("SaveTime_Text");
+                saveTimeLabel.text = new DateTime(info.SaveTime, DateTimeKind.Local).ToString();
+
+                var sceneLabel = goTable.GetNode<Text>("Scene_Text");
+                sceneLabel.text = LanguageUtils.GetStr(DBLoad.Scene.Get((int)info.Scene).m_name);
+
+                var deleteSaveButton = goTable.GetNode<UIButton>("Del_UIButton");
+                deleteSaveButton.SetLeftClickEvent(() =>
+                {
+                    UIUtlils.OpenTipsBox(
+                        LanguageUtils.GetTextDef(24),
+                        () =>
+                        {
+                            SaveManager.Instance.Del(info.ReadSaveData());
+
+                            var saveInfos = _saveInfos[self.m_saveEnum];
+                            if (self.m_saveEnum == SaveEnum.手动)
+                            {
+                                saveInfos[saveInfos.IndexOf(info)] = null;
+                            }
+                            else
+                            {
+                                saveInfos.Remove(info);
+                            }
+
+                            if (saveInfos.Count > 0)
+                            {
+                                if (_selected == info)
+                                {
+                                    _selected = saveInfos[0];
+                                }
+
+                                self.m_nextGo.SetActive(value: true);
+                            }
+                            else
+                            {
+                                _selected = null;
+                                self.m_nextGo.SetActive(value: false);
+                            }
+
+                            self.m_scrollView.UpdateData();
+                        }
+                    );
+                });
+                deleteSaveButton.gameObject.SetActive(self.m_saveEnum != SaveEnum.自动);
+
+                slotToggle.isOn = info == _selected;
+            }
+        );
+        scrollView.SetItemCountFunc(() => _saveInfos[self.m_saveEnum].Count);
+
+        var menuRect = goTable.GetNode<RectTransform>("Menus_RectTransform");
+        var menuChildGo = menuRect.GetChild(0).gameObject;
+        UIUtlils.SetContent(
+            menuRect,
+            menuChildGo,
+            3,
+            (transform, i) =>
+            {
+                UIToggle toggle = transform.GetComponent<UIToggle>();
+                toggle.SetLeftClickEvent(
+                    (isOn) =>
+                    {
+                        if (isOn)
+                        {
+                            self.m_saveEnum = (SaveEnum)i;
+                            _refreshScrollMethod.Invoke(self, []);
+                        }
+                    }
+                );
+                toggle.isOn = self.m_saveEnum == (SaveEnum)i;
+            }
+        );
+
+        return false;
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(FileWindow), nameof(FileWindow.RefreshScroll))]
+    private static bool FileWindow_RefreshScroll_Prefix(FileWindow __instance)
+    {
+        var self = __instance;
+
+        if (_saveInfos.Count == 0)
+        {
+            Debug.Log("READ METADATA FROM DISK");
+            foreach (var kind in (SaveEnum[])Enum.GetValues(typeof(SaveEnum)))
+            {
+                _saveInfos[kind] =
+                    kind == SaveEnum.手动
+                        ? [.. SaveMetadata.ReadAllFixedSlots()]
+                        : [.. SaveMetadata.ReadAll(kind)];
+            }
+        }
+
+        if (_saveInfos[self.m_saveEnum].Count > 0)
+        {
+            _selected = _saveInfos[self.m_saveEnum][0];
+            self.m_nextGo.SetActive(value: true);
+        }
+        else
+        {
+            self.m_nextGo.SetActive(value: false);
+        }
+
+        self.m_scrollView.UpdateData();
+        if (_saveInfos[self.m_saveEnum].Count > 0)
+        {
+            self.m_scrollView.ScrollTo(0);
+        }
+
+        return false;
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(FileWindow), nameof(FileWindow.OnClose))]
+    private static void FileWindow_OnClose_Postfix()
+    {
+        _selected = null;
+        _saveInfos.Clear();
     }
 }
 
